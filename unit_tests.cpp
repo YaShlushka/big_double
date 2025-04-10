9ed84e5dac9e55f74a9ace842010253058c35a5c
Egor Paderin <paderinee@gmail.com>
2025-04-10 13:48:18 +0300

    small changes

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,12 +1,15 @@
 #include "big_double.h"
 
 void Test(const bool condition, const std::string &error_text, const std::string &name) {
-	std::cout << name << ": ";
+	std::cout << name << ": "
+				 << (condition ? ("PASS") : ("FAIL: \"" + error_text + "\""))
+				 << std::endl;
+}
 
-	if(condition) {
-		std::cout << "PASS" << std::endl;
-		return;
+int main() {
+	{
+		BigDouble a("593");
+		BigDouble b("395");
+		Test(a + b == "988")
 	}
-	
-	std::cout << "fail : \"" << error_text << "\"" << std::endl;
 }
